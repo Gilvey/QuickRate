bef81a86af717545c679d3c8b0f0e4820db748e8
Gilvey <kolyagilvey@gmail.com>
2025-05-31 16:53:52 +0300

    [US06] Add fetchCurrencies method to currency exchange service
    Introduce fetchCurrencies() to CurrencyExchangeNetworkProtocol and its
    implementation in CurrencyExchangeNetworkService. This method enables
    retrieving the list of available currencies from the network client,
    supporting enhanced functionality for currency exchange operations.

--- QuickRate/QuickRate/Network/CurrencyExchange/CurrencyExchangeNetworkService/CurrencyExchangeNetworkService.swift
+++ QuickRate/QuickRate/Network/CurrencyExchange/CurrencyExchangeNetworkService/CurrencyExchangeNetworkService.swift
@@ -10,11 +10,16 @@ import Foundation
 protocol CurrencyExchangeNetworkProtocol {
     
     func fetchExchangeRate(_ model: ExchangeRequestModel) async throws -> ExchangeResponse
+    func fetchCurrencies() async throws -> [String]
 }
 
 final class CurrencyExchangeNetworkService: NetworkService, CurrencyExchangeNetworkProtocol {
     
     func fetchExchangeRate(_ model: ExchangeRequestModel) async throws -> ExchangeResponse {
         return try await client.performRequest(CurrencyExchangeProvider.Request.exchange(model: model))
+    }
+    
+    func fetchCurrencies() async throws -> [String] {
+        return try await client.performRequest(CurrencyExchangeProvider.Request.getCurrencies)
     }
 }
