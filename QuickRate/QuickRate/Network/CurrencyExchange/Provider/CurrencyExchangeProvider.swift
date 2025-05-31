bd3a34845aae0d46b7b604eab146b90f3b80f89a
Gilvey <kolyagilvey@gmail.com>
2025-05-31 16:53:29 +0300

    [US06] Update currency exchange API endpoints and add currencies request
    Refactor baseURL to include 'currency/commercial' path segment for API calls.
    Replace 'commercial' endpoint with 'exchange' for currency exchange requests.
    Add new 'getCurrencies' request case to fetch available currencies.
    Update URL construction logic to support new endpoints and requests.

--- QuickRate/QuickRate/Network/CurrencyExchange/Provider/CurrencyExchangeProvider.swift
+++ QuickRate/QuickRate/Network/CurrencyExchange/Provider/CurrencyExchangeProvider.swift
@@ -9,24 +9,28 @@ import Foundation
 
 enum CurrencyExchangeProvider {
     enum Path {
-        static let baseURL = "http://api.evp.lt/"
+        static let baseURL = "http://api.evp.lt/currency/commercial/"
         static let latest = "latest"
-        static let commercial = "currency/commercial/exchange"
+        static let exchange = "exchange"
+        static let currencies = "currencies"
     }
     
     enum Request {
         case exchange(model: ExchangeRequestModel)
+        case getCurrencies
     }
 }
 
 extension CurrencyExchangeProvider.Request: EndPoint {
     var url: URL? {
+        var path = ""
         switch self {
         case .exchange(let model):
-            let path = "\(CurrencyExchangeProvider.Path.commercial)/\(model.amount)-\(model.fromCurrency)/\(model.toCurrency)/\(CurrencyExchangeProvider.Path.latest)"
-//            http://api.evp.lt/currency/commercial/exchange/340.51-EUR/ABC/latest
-            print(CurrencyExchangeProvider.Path.baseURL + path)
-            return URL(string: CurrencyExchangeProvider.Path.baseURL + path)
+            path = "\(CurrencyExchangeProvider.Path.exchange)/\(model.amount)-\(model.fromCurrency)/\(model.toCurrency)/\(CurrencyExchangeProvider.Path.latest)"
+            
+        case .getCurrencies:
+            path = CurrencyExchangeProvider.Path.currencies
         }
+        return URL(string: CurrencyExchangeProvider.Path.baseURL + path)
     }
 }
